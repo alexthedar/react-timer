66f1da985f9de9ddebce38e47ba6eaa26c1a74cb
alexthedar <alex.castaneda@gmail.com>
2017-02-22 14:59:21 -0800

    initial

---
+++ README.md
@@ -0,0 +1,25 @@
+# React BoilerPlate
+
+###### Practice Site by Alexthedar
+
+[Click Here For Live Site]()
+
+> An app built using React, React-router, Webpack, Babel, Foundation, SCSS
+
+Install and Run:
+
+
+``` bash
+git https://github.com/alexthedar/react-timer.git
+
+cd react-timer
+
+npm install
+
+webpack
+
+npm run start
+
+```
+
+TODO:
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack')
+
+
+module.exports={
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],  //entry file for webpack to start from
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {                   // where to output.  uses dirname for root adn then places accordingly
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      applicationStyles: 'app/styles/app.scss'
+    },
+    extensions: ['', '.js', '.jsx']  //what are the extensions of the files to go through
+  },
+  module: {
+    loaders: [  //needed for babel to convert jsx and other stuff
+      {
+        loader: 'babel-loader',  //name of loader to be used
+        query: {
+          presets: ['react','es2015', 'stage-0']  //what languages will be tranlasted
+        },
+        test: /\.jsx?$/,  //regex to say only get files that use jsx at the end and put them through this loader
+        exclude: /(node_modules | bower_components)/  //what folders do i not want the babel to go through
+      }
+    ]
+  },
+  devtool: 'eval-source-map'
+}
