804f0ec97ed055618736e7d89edc62e0dc877fe3
alexthedar <alex.castaneda@gmail.com>
2017-02-22 18:16:58 -0800

    add countdown test

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,10 +6,8 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <div>
-        Main.jsx rendered
-
-        <div>
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
           {props.children}
         </div>
       </div>
